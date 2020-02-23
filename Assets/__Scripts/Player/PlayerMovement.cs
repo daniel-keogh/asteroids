fe37890b65d7883316b291b8bff1c06143f2f0ca
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-23 16:57:11 +0000

    Add linear drag to player

--- Assets/__Scripts/Player/PlayerMovement.cs
+++ Assets/__Scripts/Player/PlayerMovement.cs
@@ -9,8 +9,8 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private float forward, rotation;
 
-    [SerializeField] private float forwardSpeed = 6.0f;
-    [SerializeField] private float rotationSpeed = 6.0f;
+    [SerializeField] private float forwardSpeed = 8.0f;
+    [SerializeField] private float rotationSpeed = 5.0f;
 
     void Start()
     {
@@ -25,7 +25,11 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddRelativeForce(new Vector2(0, forward) * forwardSpeed);
-        rb.AddTorque(-rotation * rotationSpeed);
+        forward = forward * forwardSpeed;
+        var force = new Vector2(0, forward);
+        rb.AddRelativeForce(force); // Go in whatever direction the player is facing
+
+        rotation = rotation * rotationSpeed;
+        rb.AddTorque(-rotation);
     }
 }

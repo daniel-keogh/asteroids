f94d4122da64facb14294ecf70ea719782bf2c6b
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-02-26 21:15:07 +0000

    Add SceneController & SceneNames Utility

---
+++ Assets/__Scripts/Controllers/SceneController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utilities;
+
+public class SceneController : MonoBehaviour
+{
+    public void PlayOnClick()
+    {
+        SceneManager.LoadSceneAsync(SceneNames.GAME_SCENE);
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadSceneAsync(SceneNames.MAIN_MENU);
+    }
+}
--- Assets/__Scripts/WrapAround.cs
+++ Assets/__Scripts/WrapAround.cs
@@ -9,6 +9,7 @@ public class WrapAround : MonoBehaviour
 
     void Start()
     {
+        // The bottom-left of the viewport is (0,0); the top-right is (1,1).
         viewport = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
 
         sceneHeight = new Vector2(0, viewport.y * 2);
@@ -16,6 +17,11 @@ public class WrapAround : MonoBehaviour
     }
 
     void Update()
+    {
+        Wrap();
+    }
+
+    private void Wrap()
     {
         if (transform.position.y > viewport.y)
         {

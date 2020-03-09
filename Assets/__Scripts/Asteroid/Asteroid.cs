1e9b2b6f3036592711e09228a68c166ae70a0457
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-09 22:15:10 +0000

    Add basic collision detection

---
+++ Assets/__Scripts/Asteroid/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField] private int scoreValue;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var laser = other.GetComponent<Laser>();
+
+        if (laser)
+        {
+            Destroy(laser.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        var player = other.collider.GetComponent<PlayerMovement>();
+
+        if (player)
+        {
+            Destroy(player.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}

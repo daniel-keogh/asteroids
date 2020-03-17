68b6da6e3c305ccdb1a896c9ac6378835d17967c
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-17 13:38:44 +0000

    Add an explosion effect

---
+++ Assets/__Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionEffect;
+    [SerializeField] private int explosionDuration;
+
+    private void OnDestroy()
+    {
+        GameObject explosion = Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(explosion, explosionDuration);
+    }
+}

2f87ff28c857c1b02f50c46011740837a0a46a7a
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-03-17 22:43:31 +0000

    Disappear LifeIcons when player loses a life

--- Assets/__Scripts/LifeCounter.cs
+++ Assets/__Scripts/LifeCounter.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class LifeCounter : MonoBehaviour
 {
     [SerializeField] private LifeIcon lifeIconPrefab;
-    private int startingLives; // read from the GameController
+
+    private int remainingLives;
     private GameController gc;
+    private Stack<LifeIcon> lifeIcons;
 
     void Start()
     {
@@ -16,18 +18,36 @@ public class LifeCounter : MonoBehaviour
         if (gc)
         {
             // Retrieve the starting lives value
-            startingLives = gc.StartingLives;
+            remainingLives = gc.StartingLives;
+            lifeIcons = new Stack<LifeIcon>(remainingLives);
 
             CreateIcons();
+        }
+    }
+
+    void Update()
+    {
+        CheckLivesRemaining();
+    }
+
+    private void CheckLivesRemaining()
+    {
+        if (gc)
+        {
+            if (remainingLives != gc.RemainingLives)
+            {
+                Destroy(lifeIcons.Pop().gameObject);
+                remainingLives = gc.RemainingLives;
+            }
         }
     }
 
     private void CreateIcons()
     {
         // Show the appropriate number of hearts on the screen
-        for (int i = 0; i < startingLives; i++)
+        for (int i = 0; i < remainingLives; i++)
         {
-            LifeIcon icon = Instantiate(lifeIconPrefab, transform);
+            lifeIcons.Push(Instantiate(lifeIconPrefab, transform));
         }
     }
 }

e5accc059ec3d263068f6580b07c0e4f9298b7c3
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2020-04-09 13:00:10 +0100

    Save scores to JSON file

---
+++ Assets/__Scripts/Data/PlayerData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace Data
+{
+    [Serializable]
+    public class PlayerData : IComparable<PlayerData>
+    {
+        public string name;
+        public int score;
+
+        public int CompareTo(PlayerData other)
+        {
+            return this.score.CompareTo(other.score);
+        }
+
+        public override string ToString()
+        {
+            return "PlayerData: " + name + ", " + score;
+        }
+    }
+
+    [Serializable]
+    public class LeaderBoard
+    {
+        public PlayerData[] players;
+    }
+}
---
+++ Assets/__Scripts/Data/SaveSystem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace Data
+{
+    public static class SaveSystem
+    {
+        private const string LEADERBOARD_DATA_PATH = "/leaderboard.json";
+        private const int MAX_LB_SIZE = 10;
+
+        public static void SaveToLeaderBoard(PlayerData player)
+        {
+            // Read current LeaderBoard
+            LeaderBoard current = LoadLeaderBoard();
+            List<PlayerData> temp;
+
+            if (current.players != null)
+            {
+                temp = new List<PlayerData>(current.players);
+            }
+            else
+            {
+                temp = new List<PlayerData>();
+            }
+
+            if (temp.Count == MAX_LB_SIZE)
+            {
+                PlayerData min = temp.Min();
+
+                if (player.score < min.score)
+                {
+                    // Player score not high enough
+                    return;
+                }
+
+                // Remove the lowest score
+                temp.Remove(min);
+            }
+
+            temp.Add(player);
+
+            // Create the new LeaderBoard
+            LeaderBoard lb = new LeaderBoard();
+            temp.Sort();
+            temp.Reverse();
+            lb.players = temp.ToArray();
+
+            // Save to File
+            string json = JsonUtility.ToJson(lb, true);
+            File.WriteAllText(Application.dataPath + LEADERBOARD_DATA_PATH, json);
+        }
+
+        public static LeaderBoard LoadLeaderBoard()
+        {
+            try
+            {
+                string json = File.ReadAllText(Application.dataPath + LEADERBOARD_DATA_PATH);
+                return JsonUtility.FromJson<LeaderBoard>(json);
+            }
+            catch (FileNotFoundException)
+            {
+                return new LeaderBoard();
+            }
+        }
+    }
+}
